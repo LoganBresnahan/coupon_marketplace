8d788c9553bfeb931410164ef8657b5cf732966a
LoganBresnahan <loganbbres@gmail.com>
2018-08-03 10:28:36 -0500

    Refactor Instructions and Messages
    -	modified:   lib/coupon_marketplace/screens/admin.ex
    -	modified:   lib/coupon_marketplace/screens/deposit.ex
    -	modified:   lib/coupon_marketplace/screens/login.ex
    -	modified:   lib/coupon_marketplace/screens/marketplace.ex
    -	modified:   lib/coupon_marketplace/screens/new_session.ex
    -	modified:   lib/coupon_marketplace/screens/register.ex
    -	modified:   lib/coupon_marketplace/screens/user.ex
    -	modified:   lib/coupon_marketplace/utils/instructions.ex

--- lib/coupon_marketplace/utils/instructions.ex
+++ lib/coupon_marketplace/utils/instructions.ex
@@ -1,77 +1,37 @@
 defmodule CouponMarketplace.Utils.Instructions do
-  def help(:new_session) do
-    IO.puts """
-
-    Available Options:
-    "r" for register
-    "l" for login
-    "a" for admin
-    "e" for exit
-    """
-  end
-  def help(:login) do
-    IO.puts """
-
-    What would you like to do?
-    "l" for login
-    "r" for register
-    "e" for exit
-    """
-  end
-  def help(:user) do
-    IO.puts """
-
-    What would you like to do?
-    "a" add a new coupon
-    "p" for post a coupon for sale
-    "r" for request a coupon
-    "d" for deposit more money
-    "lo" for logout
-    """
-  end
-  def help(:marketplace) do
-    IO.puts """
-
-    What would you like to do?
-    "b" for buy a coupon
-    "u" for your profile
-    "lo" for logout
-    """
-  end
-
   def help(:register, data) do
     IO.puts """
     
-    *** Error attempting to register. ***
+    ********** Error attempting to register. **********
     #{readable_changeset_errors(data)}
 
     What would you like to do?
-    "r" for register
-    "l" for login
-    "e" for exit
+    "r" register
+    "l" login
+    "e" exit
     """
   end
   def help(:deposit, data) do
     IO.puts """
 
-    *** Error updating your balance ***
+    ********** Error updating your balance **********
     #{readable_changeset_errors(data)}
 
     What would you like to do?
-    "t" for try again
-    "lo" for logout
-    "p" for your profile page
+    "t" try again
+    "lo" logout
+    "u" your profile page
     """
   end
   def help(:coupon, data) do
     IO.puts """
 
-    *** Error creating coupon ***
+    ********** Error creating coupon **********
     #{readable_changeset_errors(data)}
 
     What would you like to do?
-    "lo" for logout
-    "p" for your profile page
+    "lo" logout
+    "u" your profile page
     """
   end
 
